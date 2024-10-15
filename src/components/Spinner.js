87cc647b622c499260332d9004396fa20bb875ba
Amit Dhyani <amitdhyani0334@gmail.com>
2024-10-16 00:40:33 +0530

    spinner added

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Spinner() {
   return (
-    <div className='flex items-center w-full justify-center'>
-        <div className='w-10 h-10 border-2 border-red-500 border-solid rounded-full animate-spin border-t-transparent'>
+    <div className='flex items-center w-3/5 justify-center'>
+        <div className='w-10 h-10 border-[0.2rem] border-red-600 border-solid rounded-full animate-spin border-t-transparent'>
 
         </div>
     </div>
